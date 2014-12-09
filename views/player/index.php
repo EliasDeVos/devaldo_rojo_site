d65e9b234cd3c5463abb1e5f273bc5e191cbc28c
fre <dcfrederique@gmail.com>
2014-12-09 14:30:21 +0100

    + layout, container fix

--- views/player/index.php
+++ views/player/index.php
@@ -4,44 +4,47 @@ $this->title = 'Players';
 use kartik\widgets\StarRating;
 
 ?>
-<div class="row">
-    <?php
-    foreach ($aPlayers as $oPlayer) : ?>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <div class="card">
-                <div class="title">
-                    <img class="img-circle" src="pictures/players/<?php echo $oPlayer->firstName ?>.PNG" width="11%"/>
-                    <?php echo $oPlayer->firstName;
-                    echo ' ';
-                    echo $oPlayer->lastName ?>
+<div class="container">
+    <div class="row">
+        <?php
+        foreach ($aPlayers as $oPlayer) : ?>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="title">
+                        <img class="img-circle" src="pictures/players/<?php echo $oPlayer->firstName ?>.PNG"
+                             width="11%"/>
+                        <?php echo $oPlayer->firstName;
+                        echo ' ';
+                        echo $oPlayer->lastName ?>
 
-                    <?php if ($oPlayer->isInjured) : ?>
-                        <i class="fa fa-ambulance" ></i>
-                    <?php endif;?>
+                        <?php if ($oPlayer->isInjured) : ?>
+                            <i class="fa fa-ambulance"></i>
+                        <?php endif; ?>
 
-                </div>
-                <div class="content">
-                    <label>
-                        Positie
-                    </label>
-                    <br>
-                    <?php echo $oPlayer->preferredPosition; ?>
-                    <br>
-                    <label>Doelpunten</label>
-                    <br>
-                    <?php echo count($oPlayer->goals)?>
-                    <br>
-                    <label>Wedstrijden</label>
-                    <br>
-                    <?php echo count($oPlayer->matches)?>
-                    <br>
-                    <label>Rugnummer</label>
-                    <br>
-                    <?php echo $oPlayer->playerId?>
+                    </div>
+                    <div class="content">
+                        <label>
+                            Positie
+                        </label>
+                        <br>
+                        <?php echo $oPlayer->preferredPosition; ?>
+                        <br>
+                        <label>Doelpunten</label>
+                        <br>
+                        <?php echo count($oPlayer->goals) ?>
+                        <br>
+                        <label>Wedstrijden</label>
+                        <br>
+                        <?php echo count($oPlayer->matches) ?>
+                        <br>
+                        <label>Rugnummer</label>
+                        <br>
+                        <?php echo $oPlayer->playerId ?>
 
 
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach ?>
+        <?php endforeach ?>
+    </div>
 </div>
--- views/statistics/index.php
+++ views/statistics/index.php
@@ -2,31 +2,31 @@
 
 \app\assets\JqPlotAsset::register($this);
 ?>
+    <div class="container">
+        <div id="tabs" class="tabs">
+            <ul class="nav nav-tabs" role="tablist" id="grafieken">
+                <li role="presentation" class="active"><a href="#tabs-1">Uitslagen</a></li>
+                <li role="presentation"><a href="#tabs-2">Goals/Spelers</a></li>
+            </ul>
+            <div class="tab-content">
+                <div id="tabs-1" class="tab active">
+                    <p>Uitslagen gespeelde matchen</p>
 
-	<div id="tabs" class="tabs">
-		<ul class="nav nav-tabs" role="tablist" id="grafieken">
-			<li role="presentation" class="active"><a href="#tabs-1">Uitslagen</a></li>
-			<li role="presentation"><a href="#tabs-2">Goals/Spelers</a></li>
-		</ul>
-		<div class="tab-content">
-			<div id="tabs-1" class="tab active">
-				<p>Uitslagen gespeelde matchen</p>
+                    <div id="chart1" style="width: 50%;margin-top: 50px;text-align: center">
 
-				<div id="chart1" style="width: 50%;margin-top: 50px;text-align: center">
+                    </div>
+                </div>
 
-				</div>
-			</div>
+                <div id="tabs-2" class="tab">
+                    <p>Gemaakte goals en aanwezige spelers per match</p>
 
-			<div id="tabs-2" class="tab">
-				<p>Gemaakte goals en aanwezige spelers per match</p>
+                    <div id="chart2" style="width: 50%;margin-top: 50px;text-align: center">
 
-				<div id="chart2" style="width: 50%;margin-top: 50px;text-align: center">
-
-				</div>
-			</div>
-		</div>
-	</div>
-
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 $script = <<< JS
 $(document).ready(function(){
