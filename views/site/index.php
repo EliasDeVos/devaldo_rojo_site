306946fe8e08a3e468e5ef4599f365b54e46d53f
dcfrederique <dcfrederique@gmail.com>
2014-12-14 11:56:23 +0100

    +indexlayout

--- views/site/index.php
+++ views/site/index.php
@@ -1,13 +1,12 @@
 <?php
+\app\assets\IndexAsset::register($this);
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
-use yii\helpers\Html;
-
+$this->title = 'Home';
 ?>
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-2">
+            <div class="col-lg-2 col-sm-4 col-md-4 col-xs-4">
                 <div class="row table-bordered" style="height: 150px">
                     <p>Sponsors</p>
                 </div>
@@ -15,7 +14,7 @@ use yii\helpers\Html;
                     <p>latje trap + carousel</p>
                 </div>
             </div>
-            <div class="col-lg-8 col-sm-8 col-md-8 col-xs-8">
+            <div class="col-lg-7 col-sm-4 col-md-4 col-xs-4">
                 <div class="row table-bordered" style="height: 400px">
                     <p>Logo</p>
                 </div>
@@ -23,42 +22,49 @@ use yii\helpers\Html;
                     <p>biografie</p>
                 </div>
             </div>
-            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-2">
-                <div class="row table-bordered" style="height: 300px">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Team</th>
-                            <th>Played</th>
-                            <th>Won</th>
-                            <th>Lose</th>
-                            <th>Draw</th>
-                            <th>+</th>
-                            <th>-</th>
-                            <th>Points</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <?php
-                            foreach ($aShortRankings as $iCounter => $oRanking):?>
-                            <td><?php echo $iCounter + 1 ?></td>
-                            <td><?php echo \app\models\Team::findOne(['teamId' => $oRanking->teamId])->name ?></td>
-                            <td><?php echo $oRanking->calculateMatchPlayed() ?></td>
-                            <td><?php echo $oRanking->gamesWon ?></td>
-                            <td><?php echo $oRanking->gamesLost ?></td>
-                            <td><?php echo $oRanking->gamesDraw ?></td>
-                            <td><?php echo $oRanking->goalsMade ?></td>
-                            <td><?php echo $oRanking->goalsAgainst ?></td>
-                            <td><?php echo $oRanking->calculatePoints() ?></td>
-                        </tr>
-                        <?php endforeach ?>
-                        </tbody>
-                    </table>
-                </div>
-                <div id="clock" class="row table-bordered" style="height: 150px">
+            <div class="col-lg-3 col-sm-4 col-md-4 col-xs-4">
+                <div class="row">
+                    <div class="col-md-12 ">
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="img" src="pictures/bg/KlassementBg.jpg" height="80px" width="100%">
+                                <span class="card-title">Klassement</span>
+                            </div>
 
+                            <div class="card-content">
+                                <table>
+                                    <thead>
+                                    <tr>
+                                        <th width="8%">#</th>
+                                        <th width="72%">Team</th>
+                                        <th width="15%">Pl</th>
+                                        <th width="5%">Pts</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <?php
+                                        foreach ($aShortRankings as $iCounter => $oRanking):?>
+                                        <td><?php echo $iCounter + 1 ?></td>
+                                        <td><?php echo \app\models\Team::findOne(['teamId' => $oRanking->teamId])->name ?></td>
+                                        <td><?php echo $oRanking->calculateMatchPlayed() ?></td>
+                                        <td><?php echo $oRanking->calculatePoints() ?></td>
+                                    </tr>
+                                    <?php endforeach ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div  class="row">
+                    <div class="col-md-12 ">
+                        <div class="card">
+                            <div class="card-content" id = "clock">
+                                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -69,7 +75,7 @@ $this->registerJs(
     '$("document").ready(function(){
      $("#clock").countdown("' . $sNextMatchTime . '").on("update.countdown", function(event) {
        var $this = $(this).html(event.strftime(""
-     + "<span>%-w</span> week%!w "
+       +"<h4>Volgende match in: </h4>"
      + "<span>%-d</span> day%!d "
      + "<span>%H</span> hr "
      + "<span>%M</span> min "
